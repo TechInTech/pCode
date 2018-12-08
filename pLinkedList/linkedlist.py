33411cd9feb3f14ff4be9db06546e7ae44818aee
nbpc47 <larry@DESKTOP-U3IJBEI.localdomain>
2018-12-08 15:23:16 +0800

    nothing to say

--- pLinkedList/linkedlist.py
+++ pLinkedList/linkedlist.py
@@ -33,21 +33,22 @@ class LinkedList(AbstractList):
         """To get the item at position i"""
         #Precondition: 0 <= i < len(self)
         #PostCondition: Return the item at position i
-        cursor = 0
-        tempNode = self._head.getNext()
-        if(i in range(0, len(self))):
-            while(cursor < i):
-                tempNode = tempNode.getNext()
-                cursor += 1 
-
+        tempNode = self._getItem(i)
+        if(None != tempNode):
             return tempNode.getValue()
-        else:
-            raise IndexError("Index out of range.")
 
     def __setItem__(self, i, value):
         """To set the value at postion i to the given value"""
         #Precondition 0 <= i < len(self)
         #PostCondition The value at position i is set to the given value
+        tempNode = self._getItem(i)
+        if(None != tempNode):
+            tempNode.setValue(value)
+
+    def _getItem(self, i):
+        """This function returns the two way node at position i"""
+        #Precondition: 0 <= i < len(self), or a IndexError will be raised
+        #Postcondition: returns the item at postion i
         cursor = 0
         tempNode = self._head.getNext()
         if(i in range(0, len(self))):
@@ -55,14 +56,55 @@ class LinkedList(AbstractList):
                 tempNode = tempNode.getNext()
                 cursor += 1 
 
-            tempNode.setValue(value)
+            return tempNode
         else:
             raise IndexError("Index out of range.")
 
 
     def insert(self, i, value):
-        """Insert one node at """
-        pass
+        """Insert one node at position i"""
+        nodeToInsert = TwoWayNode(value)
+        if i <= 0:
+            nodeToInsert.setNext(self._head.getNext())
+            nodeToInsert.setPrevious(self._head.getPrevious())
+            self._head.setNext(nodeToInsert)
+        elif i > len(self):
+            i = len(self)
+            self._head.getPrevious().setNext(nodeToInsert)
+            self._head.setPrevious(nodeToInsert)
+        else:
+            tempNode = self._getItem(i)
+            tempNode.setPrevious(nodeToInsert)
+            nodeToInsert.setNext(tempNode)
+            nodeToInsert.setPrevious(tempNode.getPrevious())
+
+        self._size += 1
+        self.increModCount()
+
+    def pop(self, i):
+        """Return the item at position i and remove it from the list"""
+        if i in range(0, len(self)):
+            retValue = None
+            #Pop the first node
+            if 0 == i:
+                retValue = self._head.getNext().getValue()
+                self._head.setNext(self._head.getNext().getNext())
+            #Pop the last node
+            elif i == len(self) - 1:
+                retValue = self._head.getPrevious().getValue()
+                self._head.setPrevious(self._head.getPrevious().getPrevious())
+            else:
+                tempNode = self._getItem(i)
+                retValue = tempNode.getValue()
+                tempNode.getPrevious().setNext(tempNode.getNext())
+            
+            self._size -= 1
+            self.increModCount()
+
+            return retValue
+
+        else:
+            raise IndexError("Index out of range.")
 
 #For test purpose
 def main():
