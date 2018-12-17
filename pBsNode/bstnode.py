d41eee008ba2c00fcfc0e9973bcef12a5554286b
nbpc47 <larry.yu@0110@gmail.com>
2018-12-17 22:11:23 +0800

    Adding BST

---
+++ pBsNode/bstnode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+"""
+File: bstnode.py
+Description: This file defines the class BSNode
+Author: Larry Yu
+Initial Date: Dec 8th, 2018
+"""
+
+class BSTNode(object):
+    def __init__(self):
+        pass
+
+#For test purpose
+def main():
+    pass
+
+if "__main__" == __name__:
+    main()
---
+++ pLinkedBST/linkedbst.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+"""
+File: arraylistiterator.py
+Description: This file defines the class ArrayListIterator
+Author: Larry Yu
+Initial Date: Dec 8th, 2018
+"""
+
+import sys
+sys.path.append("/mnt/d/repo/pCode/")
+from pAbstractCollection.abstractCollection import AbstractCollection
+from pBsNode.bstnode import BSTNode
+
+class LinkedBST(AbstractCollection):
+    pass
+
+def main():
+    pass
+
+if "__main__" == __name__:
+    main()
